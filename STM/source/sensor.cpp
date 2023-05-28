15eb81c6baea09aba97de2f6b408f52de814690a
MY Tu <tu890626@gmail.com>
2023-05-28 18:23:21 +0800

    Update sensor.cpp

--- STM/source/sensor.cpp
+++ STM/source/sensor.cpp
@@ -4,7 +4,8 @@
 #define SAMPLE_PERIOD 2ms
 #define ROTATION_THRESHOLD 285
 
-const int LEFT_RIGHT_THRESHOLDS[] = {96, 191, 285, 376, 465, 550, 631, 707};
+const int LEFT_RIGHT_THRESHOLDS[] = {96, 191, 285, 376, 465, 550, 631, 707, 778};
+int THRES_SIZE = sizeof(LEFT_RIGHT_THRESHOLDS) / sizeof(LEFT_RIGHT_THRESHOLDS[0]);
 
 Sensor::Sensor(events::EventQueue &event_queue) : _event_queue(event_queue) {
   BSP_ACCELERO_Init();
@@ -38,16 +39,16 @@ void Sensor::check_left_right(int8_t &move) {
   BSP_ACCELERO_AccGetXYZ(_pAccDataXYZ);
   accumulate_x = (accumulate_x + _pAccDataXYZ[0] - _AccOffset[0]) >> 1;
   if (accumulate_x > 0) {
-    for (int i = 7; i >= 0; --i) {
+    for (int i = THRES_SIZE-1; i >= 0; --i) {
       if (accumulate_x > LEFT_RIGHT_THRESHOLDS[i]) {
-        move = -i;
+        move = -i-1;
         break;
       }
     }
   } else if (accumulate_x < 0) {
-    for (int i = 7; i >= 0; --i) {
+    for (int i = THRES_SIZE-1; i >= 0; --i) {
       if (accumulate_x < -LEFT_RIGHT_THRESHOLDS[i]) {
-        move = i;
+        move = i+1;
         break;
       }
     }
